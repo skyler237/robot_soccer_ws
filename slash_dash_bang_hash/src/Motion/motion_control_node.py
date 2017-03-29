1d2e7d937e76a2c19bae7c217b3c63907245eab2
Skyler Tolman <skyler237@gmail.com>
2017-03-28 20:03:19 -0600

    Removed prints and editted PID

--- slash_dash_bang_hash/src/Motion/motion_control_node.py
+++ slash_dash_bang_hash/src/Motion/motion_control_node.py
@@ -85,7 +85,7 @@ def _handle_velocity_command(msg):
     vel_cmd_.x = msg.linear.x
     vel_cmd_.y = msg.linear.y
     vel_cmd_.z = msg.angular.z*pi/180.0
-    print(msg)
+    #print(msg)
     computeMotorSpeeds()
 
 def computeMotorSpeeds():
@@ -103,13 +103,13 @@ def computeMotorSpeeds():
     velocities = np.array([vel_cmd_.x, vel_cmd_.y, vel_cmd_.z])
     wheel_speeds_ = np.dot(M_, R).dot(velocities)
 
-    print(wheel_speeds_)
+    #print(wheel_speeds_)
     max_speed = np.amax(np.array([abs(x) for x in wheel_speeds_]))
-    print(max_speed)
+    #print(max_speed)
 
     if (max_speed < 0.2):
       wheel_speeds_ = np.array([0, 0, 0])
-    elif max_speed < 2.0:
+    elif max_speed < 3.0:
       wheel_speeds_ = np.array([x*(2.0/max_speed) for x in wheel_speeds_])
 
     sendVelocityCommands()
@@ -119,7 +119,7 @@ def sendVelocityCommands():
     speedM2 = wheel_speeds_[1] # rot/s
     speedM3 = wheel_speeds_[2] # rot/s
 
-    print(wheel_speeds_)
+    #print(wheel_speeds_)
 
     # totalTime = 3   #seconds
     # sampleRate = 50 #samples per second
@@ -144,10 +144,10 @@ def main():
 
 
     # Set the PIDQ values for all motors
-    #setPID(0, 1, 0.3, 30000)
-    setPID(1, -1, -0.4, 49000)
-    setPID(2, -1, -0.4, 48000)
-    setPID(3, -1, -0.4, 50000)
+    setPID(0, 1.3, 0.55, 50000)
+    #setPID(1, -1, -0.4, 49000)
+    #setPID(2, -1, -0.4, 48000)
+    #setPID(3, -1, -0.4, 50000)
 
     # Set tick period (triggers PID control) and velocity filter corner frequency
     setT(20, 50)
